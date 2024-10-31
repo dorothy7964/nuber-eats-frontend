ff096fb897629a3a3655990dbeaba29a5fa70a57
wam <dorothy_007@naver.com>
2024-10-31 22:08:17 +0900

    Header Test,  공통된 렌더링 로직을 분리

--- src/components/__tests__/header.spec.tsx
+++ src/components/__tests__/header.spec.tsx
@@ -4,33 +4,37 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Header } from "../header";
 import { ME_QUERY } from "../../hooks/useMe";
 
-describe("<Header />", () => {
-  it("이메일 인증이 되지 않은 경우에는 배너가 표시되어야 합니다.", async () => {
-    const { getByText } = render(
-      <MockedProvider
-        mocks={[
-          {
-            request: {
-              query: ME_QUERY
-            },
-            result: {
-              data: {
-                me: {
-                  id: 1,
-                  email: "",
-                  role: "",
-                  verified: false
-                }
+const renderWithMeQuery = (verified: boolean) => {
+  return render(
+    <MockedProvider
+      mocks={[
+        {
+          request: {
+            query: ME_QUERY
+          },
+          result: {
+            data: {
+              me: {
+                id: 1,
+                email: "email",
+                role: "role",
+                verified
               }
             }
           }
-        ]}
-      >
-        <Router>
-          <Header />
-        </Router>
-      </MockedProvider>
-    );
+        }
+      ]}
+    >
+      <Router>
+        <Header />
+      </Router>
+    </MockedProvider>
+  );
+};
+
+describe("<Header />", () => {
+  it("이메일 인증이 되지 않은 경우에는 배너가 표시되어야 합니다.", async () => {
+    const { getByText } = renderWithMeQuery(false);
 
     await waitFor(() => {
       getByText("이메일을 확인하시기 바랍니다.");
@@ -38,31 +42,7 @@ describe("<Header />", () => {
   });
 
   it("이메일 인증이 완료된 경우에는 배너가 표시되지 않아야 합니다.", async () => {
-    const { queryByText } = render(
-      <MockedProvider
-        mocks={[
-          {
-            request: {
-              query: ME_QUERY
-            },
-            result: {
-              data: {
-                me: {
-                  id: 1,
-                  email: "",
-                  role: "",
-                  verified: true
-                }
-              }
-            }
-          }
-        ]}
-      >
-        <Router>
-          <Header />
-        </Router>
-      </MockedProvider>
-    );
+    const { queryByText } = renderWithMeQuery(true);
 
     // 이메일 인증 상태에 따라 배너가 표시되지 않아야 함을 기다린다.
     await waitFor(() => {
