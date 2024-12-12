ff5ca1f99b33824c23121fdc15cfaf82fab398e5
wam <dorothy_007@naver.com>
2024-12-12 23:22:03 +0900

    22.10 DishOptions part One ~ 22.11 DishOptions part Two

--- src/pages/owner/add-dish.tsx
+++ src/pages/owner/add-dish.tsx
@@ -1,14 +1,16 @@
 import { gql, useMutation } from "@apollo/client";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useHistory, useParams } from "react-router-dom";
-import { Button } from "../../components/button";
-import { FormError } from "../../components/form-error";
-import { PageMeta } from "../../components/pageMeta ";
-import { MY_RESTAURANT_QUERY } from "./my-restaurant";
+import { v4 as uuidv4 } from "uuid"; // uuid 라이브러리에서 v4 UUID 함수 가져오기
 import {
   CreateDishMutation,
   CreateDishMutationVariables
 } from "../../__generated__/types";
+import { Button } from "../../components/button";
+import { FormError } from "../../components/form-error";
+import { PageMeta } from "../../components/pageMeta ";
+import { MY_RESTAURANT_QUERY } from "./my-restaurant";
 
 const CREATE_DISH_MUTATION = gql`
   mutation createDish($input: CreateDishInput!) {
@@ -27,6 +29,7 @@ interface IFormProps {
   name: string;
   price: number;
   description: string;
+  [key: string]: string | number; // 동적 속성 처리
 }
 
 export const AddDish: React.FC = () => {
@@ -52,6 +55,7 @@ export const AddDish: React.FC = () => {
   const {
     register,
     getValues,
+    setValue,
     formState: { isValid, errors },
     handleSubmit
   } = useForm<IFormProps>({
@@ -59,7 +63,13 @@ export const AddDish: React.FC = () => {
   });
 
   const onSubmit = () => {
-    const { name, price, description } = getValues();
+    const { name, price, description, ...rest } = getValues();
+
+    // 옵션 추가 object 형식의 배열로 변환 / 예: [{}, {}]
+    const optionList = optionIds.map((theId: string) => ({
+      name: rest[`${theId}-optionName`] as string,
+      extra: +rest[`${theId}-optionExtra`]
+    }));
 
     createDishMutation({
       variables: {
@@ -67,12 +77,26 @@ export const AddDish: React.FC = () => {
           name,
           price,
           description,
-          restaurantId: +restaurantId
+          restaurantId: +restaurantId,
+          options: optionList
         }
       }
     });
 
     history.goBack();
+  };
+
+  const [optionIds, setOptionIds] = useState<string[]>([]); // 상태는 UUID 문자열 배열
+
+  const onAddOptionClick = () => {
+    const UUID = uuidv4().replace(/-/g, ""); // 하이픈 제거
+    setOptionIds((current) => [UUID, ...current]); // UUID 생성 후 배열 맨 앞에 추가
+  };
+
+  const onDeleteClick = (idToDelete: string) => {
+    setOptionIds((current) => current.filter((id) => id !== idToDelete));
+    setValue(`${idToDelete}-optionName`, "");
+    setValue(`${idToDelete}-optionExtra`, "");
   };
 
   return (
@@ -104,13 +128,15 @@ export const AddDish: React.FC = () => {
               message: "가격은 0 이상이어야 합니다."
             }
           })}
+          min="0" // HTML input의 min 속성 추가
+          step="100" // 100원 단위 설정
           className="input"
           type="number"
           placeholder="가격"
         />
         {errors.price?.message && (
           <FormError errorMessage={errors.price?.message} />
-        )}
+        )}{" "}
         <input
           {...register("description", {
             required: "음식의 설명을 입력해 주세요."
@@ -122,6 +148,70 @@ export const AddDish: React.FC = () => {
         {errors.description?.message && (
           <FormError errorMessage={errors.description?.message} />
         )}
+        {/* 옵션 추가 */}
+        <div className="my-10">
+          <h4 className="font-semibold text-xl mb-3">요리 옵션</h4>
+          <span
+            onClick={onAddOptionClick}
+            className=" cursor-pointer text-white bg-gray-900 py-1 px-2 mt-5"
+          >
+            요리 옵션 추가
+          </span>
+          {optionIds.length !== 0 &&
+            optionIds.map((uuid: string) => (
+              <div key={uuid} className="mt-5">
+                <input
+                  {...register(`${uuid}-optionName`, {
+                    required: "옵션 이름을을 입력해 주세요."
+                  })}
+                  type="text"
+                  placeholder="옵션 이름"
+                  className="input-sm mr-3"
+                />
+                <input
+                  {...register(`${uuid}-optionExtra`, {
+                    required: "옵션 가격을을 입력해 주세요.",
+                    min: {
+                      value: 0,
+                      message: "가격은 0 이상이어야 합니다."
+                    }
+                  })}
+                  min="0" // HTML input의 min 속성 추가
+                  step="100" // 100원 단위 설정
+                  type="number"
+                  placeholder="옵션 가격"
+                  className="input-sm"
+                />
+                <button
+                  role="button"
+                  type="button"
+                  className="cursor-pointer text-white bg-red-500 ml-3 py-3 px-4 mt-5"
+                  onClick={() => onDeleteClick(uuid)}
+                >
+                  옵션 삭제
+                </button>
+                {/* 오류 메시지 출력 */}
+                <p>
+                  {errors[`${uuid}-optionName`] && (
+                    <FormError
+                      errorMessage={
+                        errors[`${uuid}-optionName`]?.message as string
+                      }
+                    />
+                  )}
+                </p>
+                <p>
+                  {errors[`${uuid}-optionExtra`] && (
+                    <FormError
+                      errorMessage={
+                        errors[`${uuid}-optionExtra`]?.message as string
+                      }
+                    />
+                  )}
+                </p>
+              </div>
+            ))}
+        </div>
         <Button loading={loading} canClick={isValid} actionText="요리 추가" />
       </form>
     </div>
