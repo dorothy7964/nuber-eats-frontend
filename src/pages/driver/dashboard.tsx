84785ad2aa0d9b9d6c56bc4e7a860458698a0c39
wam <dorothy_007@naver.com>
2025-03-05 20:23:34 +0900

    25.6 Driver Dashboard part Two

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,7 +1,42 @@
 import GoogleMapReact from "google-map-react";
+import { useEffect, useState } from "react";
+
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY || "";
+
+interface ICoords {
+  lat: number;
+  lng: number;
+}
 
 export const Dashboard = () => {
-  const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY || "";
+  const [driverCoords, setDriverCoords] = useState<ICoords>({
+    lat: 37.7298624,
+    lng: 127.1092041
+  });
+
+  // @ts-ignore
+  const onSucces = ({ coords: { latitude, longitude } }: Position) => {
+    setDriverCoords({ lat: latitude, lng: longitude });
+  };
+  // @ts-ignore
+  const onError = (error: PositionError) => {
+    console.log(error);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSucces, onError, {
+      enableHighAccuracy: true
+    });
+  }, []);
+
+  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    // map: 현재 표시되는 지도 객체
+    // maps: Google Maps JavaScript API 전체 객체
+
+    setTimeout(() => {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    }, 3000);
+  };
 
   return (
     <>
@@ -10,8 +45,10 @@ export const Dashboard = () => {
         style={{ width: window.innerWidth, height: "50vh" }}
       >
         <GoogleMapReact
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={onApiLoaded}
           defaultZoom={16}
-          defaultCenter={{ lat: 37.5665, lng: 126.978 }} // 중심 좌표
+          defaultCenter={{ lat: 36.7298624, lng: 127.1092041 }} // 중심 좌표
           bootstrapURLKeys={{ key: apiKey }}
         />
       </div>
