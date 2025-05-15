bb3f0dd0f3d387069dc0ce9f0aedf30018fc1a41
wam <dorothy_007@naver.com>
2025-05-15 18:57:27 +0900

    이메일 인증 배너 제거

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,12 +9,6 @@ export const Header: React.FC = () => {
 
   return (
     <>
-      {!data?.me.verified && (
-        <div className="bg-red-500 p-3 text-center text-base text-white">
-          <span>이메일을 확인하시기 바랍니다.</span>
-        </div>
-      )}
-
       <header className="py-4">
         <div className="w-full px-5 xl:px-0 max-w-screen-2xl mx-auto flex justify-between items-center">
           <Link to="/">
